3d6e1e0375cae4540810e72172cca5772bd989ca
Eugene Wolffe <fluffy1781@gmail.com>
2023-12-27 16:45:56 -0500

    Give String Names to the effects

--- cheshire/generic/effect.py
+++ cheshire/generic/effect.py
@@ -3,18 +3,18 @@ from enum import Enum
 class Effect(Enum):
     """Generic effects which may be interpreted differently or not at all
     by different devices"""
-    NONE = -1
-    JUMP_7 = 0
-    JUMP_3 = 1
-    FADE_7 = 2
-    FADE_3 = 3
-    FLASH  = 4
-    AUTO   = 5
+    NONE = "None"
+    JUMP_7 = "Jump 7"
+    JUMP_3 = "Jump 3"
+    FADE_7 = "Fade 7"
+    FADE_3 = "Fade 3"
+    FLASH  = "Flash"
+    AUTO   = "Auto"
     # Slowly fades through colors. Noise increases speed.
-    FADE_7_FAST_ON_NOISE = 600
+    FADE_7_FAST_ON_NOISE = "Fade 7 Volume Controls Speed"
     # Slowly pulses each color twice. Noise increases speed.
-    TWO_FADE_FAST_ON_NOISE = 700
+    TWO_FADE_FAST_ON_NOISE = "Two Fade Volume Controls Speed"
     # Solid color. Noise quickly cycles through colors.
-    JUMP_ON_NOISE_PAUSE_QUIET = 800
+    JUMP_ON_NOISE_PAUSE_QUIET = "Cycle Colors When Loud"
     # Off. Noise turns on lights and quickly cycles through colors.
-    JUMP_ON_NOISE_OFF_QUIET = 900
+    JUMP_ON_NOISE_OFF_QUIET = "Light and Cycle Colors When Loud"
